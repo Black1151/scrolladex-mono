6fc425e9ab0b2bbe6ec9f8b5007d6f7278fb27ed
blackPrawn <danblack9988@gmail.com>
2023-06-09 16:46:38 +0100

    set up redux provider

--- scrolladex-fe/pages/_app.tsx
+++ scrolladex-fe/pages/_app.tsx
@@ -3,14 +3,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme/theme";
 import Navbar from "@/components/layout/Navbar";
 import { AuthProvider } from "@/providers/AuthProvider";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <AuthProvider>
-        <Navbar />
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <AuthProvider>
+          <Navbar />
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ChakraProvider>
+    </Provider>
   );
 }
