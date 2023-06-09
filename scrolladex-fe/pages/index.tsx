d341b3fdc0a96e6866d6106a58d6375c78929407
blackPrawn <danblack9988@gmail.com>
2023-06-09 16:48:08 +0100

    create emploupdate index.ts to use redux for fetching employee overview

--- scrolladex-fe/pages/index.tsx
+++ scrolladex-fe/pages/index.tsx
@@ -10,43 +10,26 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import { getEmployeesOverviewAPI } from "../api/employeeApi";
-import { EmployeeOverview } from "../types";
 import EmployeeDetailsModal from "@/components/modals/EmployeeDetailsModal";
 import { Employee } from "../types";
 import { getEmployeeAPI } from "../api/employeeApi";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "@/store/store";
+import { fetchEmployeeOverview } from "@/store/employeeSlice";
 
 const MotionBox = motion(Box);
 
 const Index = () => {
-  const [employees, setEmployees] = useState<EmployeeOverview[]>([]);
+  const dispatch: AppDispatch = useDispatch();
+  const employees = useSelector((state: RootState) => state.employee.entities);
   const [employee, setEmployee] = useState<Employee | null>(null);
   const [loaded, setLoaded] = useState<boolean[]>([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toast = useToast();
 
-  const fetchEmployees = async () => {
-    try {
-      const data = await getEmployeesOverviewAPI();
-      if (data) {
-        setEmployees(data);
-        setLoaded(new Array(data.length).fill(false));
-      }
-    } catch (error: any) {
-      toast({
-        title: "Error",
-        description: `Failed to call API: ${error.message}`,
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-      });
-      throw error;
-    }
-  };
-
   useEffect(() => {
-    fetchEmployees();
-  }, []);
+    dispatch(fetchEmployeeOverview());
+  }, [dispatch]);
 
   const handleImageLoad = (index: number) => {
     setLoaded((prev) => {
@@ -90,56 +73,59 @@ const Index = () => {
         position="relative"
       >
         {employees.map((employee, index) => (
-          <MotionBox
-            key={employee.id}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: loaded[index] ? 1 : 0 }}
-            transition={{ duration: Math.random() * 1 }}
-            onClick={() => handleCardClick(employee.id)}
-            cursor="pointer"
-          >
-            <Box
-              bg="white"
-              p={5}
-              shadow="md"
-              borderWidth="1px"
-              transition="all 0.25s"
-              transform="scale(1)"
-              zIndex={0}
-              position="relative"
-              _hover={{
-                transform: "scale(1.025)",
-                shadow: "xl",
-                zIndex: 10,
-              }}
+          <>
+            {console.log(employee.profilePictureUrl)}
+            <MotionBox
+              key={employee.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: loaded[index] ? 1 : 0 }}
+              transition={{ duration: Math.random() * 1 }}
+              onClick={() => handleCardClick(employee.id)}
+              cursor="pointer"
             >
-              <Flex justifyContent="space-between">
-                <Box whiteSpace="nowrap">
-                  <Heading fontSize="xl">
-                    {employee.firstName} {employee.lastName}
-                  </Heading>
-                  <Text mt={2} fontSize="sm">
-                    {employee.jobTitle}
-                  </Text>
-                  <Text mt={2} fontSize="sm">
-                    {employee.departmentName}
-                  </Text>
-                </Box>
-                <Box mr={4}>
-                  <Image
-                    borderRadius="full"
-                    boxSize="100px"
-                    src={`${process.env.NEXT_PUBLIC_SERVER_ADDRESS!}/public${
-                      employee.profilePictureUrl || "/placeholder.png"
-                    }`}
-                    alt={employee.firstName + " " + employee.lastName}
-                    fallbackSrc="/placeholder.png"
-                    onLoad={() => handleImageLoad(index)}
-                  />
-                </Box>
-              </Flex>
-            </Box>
-          </MotionBox>
+              <Box
+                bg="white"
+                p={5}
+                shadow="md"
+                borderWidth="1px"
+                transition="all 0.25s"
+                transform="scale(1)"
+                zIndex={0}
+                position="relative"
+                _hover={{
+                  transform: "scale(1.025)",
+                  shadow: "xl",
+                  zIndex: 10,
+                }}
+              >
+                <Flex justifyContent="space-between">
+                  <Box whiteSpace="nowrap">
+                    <Heading fontSize="xl">
+                      {employee.firstName} {employee.lastName}
+                    </Heading>
+                    <Text mt={2} fontSize="sm">
+                      {employee.jobTitle}
+                    </Text>
+                    <Text mt={2} fontSize="sm">
+                      {employee.departmentName}
+                    </Text>
+                  </Box>
+                  <Box mr={4}>
+                    <Image
+                      borderRadius="full"
+                      boxSize="100px"
+                      src={`${process.env.NEXT_PUBLIC_SERVER_ADDRESS!}/public${
+                        employee.profilePictureUrl || "/placeholder.png"
+                      }`}
+                      alt={employee.firstName + " " + employee.lastName}
+                      fallbackSrc="/placeholder.png"
+                      onLoad={() => handleImageLoad(index)}
+                    />
+                  </Box>
+                </Flex>
+              </Box>
+            </MotionBox>
+          </>
         ))}
       </SimpleGrid>
       {employee && (
@@ -147,7 +133,7 @@ const Index = () => {
           isOpen={isOpen}
           onClose={onClose}
           employee={employee}
-          fetchEmployees={fetchEmployees}
+          fetchEmployees={fetchEmployeeOverview}
         />
       )}
     </MotionBox>
