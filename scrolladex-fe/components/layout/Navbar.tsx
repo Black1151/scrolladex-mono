3a8884a4d881ae616fce8873abd14bef5c085f25
blackPrawn <danblack9988@gmail.com>
2023-06-19 14:31:12 +0100

    remove data fetching and susbmit handler logic from navbar as is now handled via hooks / redux.

--- scrolladex-fe/components/layout/Navbar.tsx
+++ scrolladex-fe/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Box,
   Flex,
@@ -14,21 +14,16 @@ import {
   useDisclosure,
   VStack,
   HStack,
-  useToast,
   Button,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import AddEmployeeModal from "../modals/AddEmployeeModal";
 import AddDepartmentModal from "../modals/AddDepartmentModal";
-import { getDepartmentsAPI } from "@/api/departmentAPI";
-import { DepartmentListItem } from "@/types";
-import { motion } from "framer-motion";
-import OutcomeModal from "../modals/OutcomeModal";
 import { logoutUserAPI } from "@/api/authAPI";
 import { useRouter } from "next/router";
 import { useAuth } from "@/providers/AuthProvider";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   const {
     onOpen: addEmployeeOnOpen,
     isOpen: addEmployeeIsOpen,
@@ -40,78 +35,14 @@ const Navbar = () => {
     onClose: addDepartmentOnClose,
   } = useDisclosure();
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [departmentList, setDepartmentList] = useState<DepartmentListItem[]>(
-    []
-  );
-  const [outcomeMessage, setOutcomeMessage] = useState("");
-  const [outcomeStatus, setOutcomeStatus] = useState<"success" | "error">(
-    "success"
-  );
-  const [isOutcomeOpen, setOutcomeOpen] = useState(false);
+  const {
+    isOpen: drawerIsOpen,
+    onOpen: drawerOnOpen,
+    onClose: drawerOnClose,
+  } = useDisclosure();
+
   const { authenticated, setAuthenticated } = useAuth();
-  const toast = useToast();
-  const MotionBox = motion(Box);
   const router = useRouter();
-
-  const createDepartmentDropdownList = async (): Promise<void> => {
-    try {
-      const departments = await getDepartmentsAPI();
-      if (departments != null) {
-        const newDepartmentsArray = departments.map((department) => ({
-          id: department.id as number,
-          departmentName: department.departmentName,
-        }));
-        setDepartmentList(newDepartmentsArray);
-      }
-    } catch (error: any) {
-      toast({
-        title: "An error occurred.",
-        description: error.message,
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-      });
-    }
-  };
-
-  const createOnSubmitHandler =
-    (
-      apiFunction: (values: any) => Promise<any>,
-      successMessage: string,
-      errorMessage: string
-    ) =>
-    async (values: any, actions: any) => {
-      try {
-        await apiFunction(values);
-        createDepartmentDropdownList();
-        showOutcomeModal("success", successMessage);
-        actions.setSubmitting(false);
-      } catch (error) {
-        console.error(error);
-        if (error instanceof Error) {
-          showOutcomeModal(
-            "error",
-            error.message || "An unexpected error occurred."
-          );
-        } else {
-          showOutcomeModal("error", errorMessage);
-        }
-      } finally {
-        actions.setSubmitting(false);
-      }
-    };
-
-  const showOutcomeModal = (status: "success" | "error", message: string) => {
-    setOutcomeStatus(status);
-    setOutcomeMessage(message);
-    setOutcomeOpen(true);
-  };
-
-  useEffect(() => {
-    createDepartmentDropdownList();
-  }, []);
-
   return (
     <>
       <Flex
@@ -139,10 +70,17 @@ const Navbar = () => {
               isOpen={addEmployeeIsOpen}
               onClose={addEmployeeOnClose}
             />
+            <AddDepartmentModal
+              isOpen={addDepartmentIsOpen}
+              onClose={addDepartmentOnClose}
+            />
             <Button variant="green" onClick={addEmployeeOnOpen}>
               Add Employee
             </Button>
-            <AddDepartmentModal createOnSubmitHandler={createOnSubmitHandler} />
+            <Button variant="green" onClick={addDepartmentOnOpen}>
+              Add Department
+            </Button>
+
             <Button
               onClick={() => {
                 logoutUserAPI();
@@ -159,28 +97,29 @@ const Navbar = () => {
           <IconButton
             aria-label="Open menu"
             icon={<HamburgerIcon />}
-            onClick={onOpen}
+            onClick={drawerOnOpen}
           />
-          <Drawer isOpen={isOpen} onClose={onClose} placement="right">
+          <Drawer
+            isOpen={drawerIsOpen}
+            onClose={drawerOnClose}
+            placement="right"
+          >
             <DrawerOverlay />
             <DrawerContent bg="blue">
               <DrawerCloseButton />
               <DrawerHeader>Menu</DrawerHeader>
               <DrawerBody>
-                <AddDepartmentModal
-                  createOnSubmitHandler={createOnSubmitHandler}
-                />
+                <Button variant="green" onClick={addEmployeeOnOpen}>
+                  Add Employee
+                </Button>
+                <Button variant="green" onClick={addDepartmentOnOpen}>
+                  Add Department
+                </Button>
               </DrawerBody>
             </DrawerContent>
           </Drawer>
         </Box>
       </Flex>
-      <OutcomeModal
-        isOpen={isOutcomeOpen}
-        onClose={() => setOutcomeOpen(false)}
-        status={outcomeStatus}
-        bodyText={outcomeMessage}
-      />
     </>
   );
 };
