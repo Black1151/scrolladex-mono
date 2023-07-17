7fd00c38bb53140ca57aa19546115c300499f536
blackPrawn <danblack9988@gmail.com>
2023-07-17 10:47:57 +0100

    updates to docker and nginx to get it working on ec2 instance

--- scrolladex-fe/utils/checkUserAuthentication.ts
+++ scrolladex-fe/utils/checkUserAuthentication.ts
@@ -2,27 +2,20 @@ import { GetServerSidePropsContext } from 'next';
 
 
 export async function checkUserAuthentication(context: GetServerSidePropsContext) {
-    try {
-      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; //////////////////////////// REMOVE FOR PRODUCTION
-  
-      const response = await fetch('https://nginx/api/check-session', {
-        headers: {
-          'Cookie': context.req.headers.cookie || ''
-        },
-      });
-  
-      const responseData = await response.json();
-  
-      if (!responseData.isAuthenticated) { 
-        return {
-          redirect: {
-            destination: '/login',
-            permanent: false,
-          },
-        };
-      }
-    } catch (error) {
-      console.error('Failed to check authentication', error);
+  try {
+    if (process.env.NODE_ENV !== "production") {
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    }
+
+    const response = await fetch(process.env.NEXT_PUBLIC_SERVER_ADDRESS + '/api/check-session', {
+      headers: {
+        'Cookie': context.req.headers.cookie || ''
+      },
+    });
+
+    const responseData = await response.json();
+
+    if (!responseData.isAuthenticated) {
       return {
         redirect: {
           destination: '/login',
@@ -30,8 +23,16 @@ export async function checkUserAuthentication(context: GetServerSidePropsContext
         },
       };
     }
+  } catch (error) {
+    console.error('Failed to check authentication', error);
     return {
-      props: {},
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
     };
   }
-  
+  return {
+    props: {},
+  };
+}
